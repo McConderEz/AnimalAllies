0ad2a5dc842d08b43991cd1bd42db19a033350f1
McConderEz <kamazitun.jaykehd@gmail.com>
2024-08-09 02:56:08 +0300

    added volunteer

--- backend/src/AnimalAllies.Domain/Constraints/Constraints.cs
+++ backend/src/AnimalAllies.Domain/Constraints/Constraints.cs
@@ -7,5 +7,6 @@ public static partial class Constraints
     public static readonly int MAX_PET_COLOR_LENGTH = 50;
     public static readonly int MAX_PET_INFORMATION_LENGTH = 250;
     public static readonly double MIN_VALUE = 0;
-    
+    public static readonly int MAX_URL_LENGTH = 2048;
+
 }
---
+++ backend/src/AnimalAllies.Domain/Models/Volunteer.cs
@@ -0,0 +1,129 @@
+using System.Reflection;
+using AnimalAllies.Domain.ValueObjects;
+using CSharpFunctionalExtensions;
+
+namespace AnimalAllies.Domain.Models;
+
+public class Volunteer: Entity
+{
+    private List<Requisite> _requisites = [];
+    private List<Pet> _pets = [];
+    
+    private Volunteer(
+        FullName fullName,
+        string description,
+        int workExperience,
+        int petsNeedsHelp,
+        int petsSearchingHome,
+        int petsFoundHome,
+        PhoneNumber phone,
+        SocialNetwork socialNetwork,
+        List<Requisite> requisites,
+        List<Pet> pets)
+    {
+        FullName = fullName;
+        Description = description;
+        WorkExperience = workExperience;
+        PetsNeedsHelp = petsNeedsHelp;
+        PetsSearchingHome = petsSearchingHome;
+        PetsFoundHome = petsFoundHome;
+        Phone = phone;
+        SocialNetwork = socialNetwork;
+        AddRequisites(requisites);
+        AddPets(pets);
+    }
+    
+    public FullName FullName { get; }
+    public string Description { get; } = string.Empty;
+    public int WorkExperience { get; }
+    public int PetsNeedsHelp { get; }
+    public int PetsSearchingHome { get; }
+    public int PetsFoundHome { get; }
+    public PhoneNumber Phone { get; }
+    public SocialNetwork SocialNetwork { get; }
+
+    public IReadOnlyList<Requisite> Requisites => _requisites;
+    public IReadOnlyList<Pet> Pets => _pets;
+
+    public void AddRequisites(List<Requisite> requisites) => _requisites.AddRange(requisites);
+    public void AddPets(List<Pet> pets) => _pets.AddRange(pets);
+
+    public static Result<Volunteer> Create(
+        string firstName,
+        string secondName,
+        string patronymic,
+        string description,
+        int workExperience,
+        int petsNeedHelp,
+        int petsSearchingHome,
+        int petsFoundHome,
+        string phoneNumber,
+        string socialNetworkTitle,
+        string socialNetworkUrl,
+        List<Requisite> requisites,
+        List<Pet> pets)
+    {
+        if (string.IsNullOrWhiteSpace(description) ||
+            description.Length < Constraints.Constraints.MAX_DESCRIPTION_LENGTH)
+        {
+            return Result.Failure<Volunteer>(
+                $"{description} cannot be null or have length more than {Constraints.Constraints.MAX_DESCRIPTION_LENGTH}");
+        }
+        
+        if (workExperience < Constraints.Constraints.MIN_VALUE)
+        {
+            return Result.Failure<Volunteer>($"{workExperience} cannot be less than {Constraints.Constraints.MIN_VALUE}");
+        }
+        
+        if (petsNeedHelp < Constraints.Constraints.MIN_VALUE)
+        {
+            return Result.Failure<Volunteer>($"{petsNeedHelp} cannot be less than {Constraints.Constraints.MIN_VALUE}");
+        }
+
+        if (petsSearchingHome < Constraints.Constraints.MIN_VALUE)
+        {
+            return Result.Failure<Volunteer>($"{petsSearchingHome} cannot be less than {Constraints.Constraints.MIN_VALUE}");
+        }
+        
+        if (petsFoundHome < Constraints.Constraints.MIN_VALUE)
+        {
+            return Result.Failure<Volunteer>($"{petsFoundHome} cannot be less than {Constraints.Constraints.MIN_VALUE}");
+        }
+
+        var fullName = FullName.Create(firstName, secondName, patronymic);
+
+        if (fullName.IsFailure)
+        {
+            return Result.Failure<Volunteer>(fullName.Error);
+        }
+
+        var phone = PhoneNumber.Create(phoneNumber);
+
+        if (phone.IsFailure)
+        {
+            return Result.Failure<Volunteer>(phone.Error);
+        }
+
+        var socialNetwork = SocialNetwork.Create(socialNetworkTitle, socialNetworkUrl);
+
+        if (socialNetwork.IsFailure)
+        {
+            return Result.Failure<Volunteer>(socialNetwork.Error);
+        }
+
+        var volunteer = new Volunteer(
+            fullName.Value,
+            description,
+            workExperience,
+            petsNeedHelp,
+            petsSearchingHome,
+            petsFoundHome,
+            phone.Value,
+            socialNetwork.Value,
+            requisites ?? [],
+            pets ?? []);
+
+        return Result.Success(volunteer);
+    }
+
+}
