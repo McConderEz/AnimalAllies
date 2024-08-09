441bdf9f4b86d468c4fc8c41023d7950d06eb378
McConderEz <kamazitun.jaykehd@gmail.com>
2024-08-09 03:32:21 +0300

    override Equals,GetHashCode,==,!= in Entity

--- backend/src/AnimalAllies.Domain/Models/Entity.cs
+++ backend/src/AnimalAllies.Domain/Models/Entity.cs
@@ -9,4 +9,38 @@ public abstract class Entity
     protected Entity() {}
     
     protected Entity(Guid id) => Id = id;
+    
+    public override bool Equals(object? obj)
+    {
+        if(obj == null)
+            return false;
+
+        if (GetType() != obj.GetType())
+            return false;
+
+        var entity = (Entity)obj;
+        
+        if (!ReferenceEquals(this, entity))
+            return false;
+
+        return Id == entity.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+
+    public static bool operator ==(Entity? a, Entity? b)
+    {
+        if (a is null && b is null)
+            return true;
+
+        if (a is null || b is null)
+            return false;
+
+        return a.Equals(b);
+    }
+
+    public static bool operator !=(Entity a, Entity b) => !(a == b);
 }
