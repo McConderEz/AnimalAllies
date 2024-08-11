da497b8af8a4a37ace457f4f4b04cf4ebfc304dd
McConderEz <kamazitun.jaykehd@gmail.com>
2024-08-11 22:09:33 +0300

    fix

--- backend/src/AnimalAllies.Domain/Models/Breed.cs
+++ backend/src/AnimalAllies.Domain/Models/Breed.cs
@@ -14,7 +14,7 @@ public class Breed: Entity
     public string Name { get; private set; }
     
     
-    public static Result<Breed> Create(string name, List<Species> speciesList)
+    public static Result<Breed> Create(string name)
     {
         if (string.IsNullOrWhiteSpace(name) || name.Length > Constraints.Constraints.MAX_VALUE_LENGTH)
             return Result.Failure<Breed>($"{nameof(name)} cannot be null or length more than {Constraints.Constraints.MAX_VALUE_LENGTH}");
