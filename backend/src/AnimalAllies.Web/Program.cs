ff1dc4580f766bce90c88648a53899427344bef6
McConderEz <kamazitun.jaykehd@gmail.com>
2024-10-09 02:40:39 +0300

    added accounts module

--- backend/src/AnimalAllies.Web/Program.cs
+++ backend/src/AnimalAllies.Web/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AnimalAllies.Web.Extensions;
 using AnimalAllies.Web.Middlewares;
 using AnimalAllies.Species.Application;
@@ -7,6 +8,9 @@ using Serilog;
 using AnimalAllies.Species.Infrastructure;
 using AnimalAllies.Species.Presentation;
 using AnimalAllies.Volunteer.Presentation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,11 +30,57 @@ builder.Services
     .AddSpeciesApplication()
     .AddSpeciesInfrastructure(builder.Configuration);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("test")),
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = false,
+            ClockSkew = TimeSpan.Zero
+        };
+        
+    });
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "My API",
+        Version = "v1"
+    });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please insert JWT with Bearer into field",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            []
+        }
+    });
+});
+
 
 
 var app = builder.Build();
@@ -51,6 +101,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
