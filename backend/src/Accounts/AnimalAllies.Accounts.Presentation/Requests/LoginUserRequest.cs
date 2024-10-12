8c8b39f6b7973ae137198a9bc4aa89163cd56b7b
McConderEz <kamazitun.jaykehd@gmail.com>
2024-10-12 22:43:11 +0300

    fixes

--- backend/src/Accounts/AnimalAllies.Accounts.Presentation/Requests/LoginUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using AnimalAllies.Accounts.Application.AccountManagement.Commands.Login;
-
-namespace AnimalAllies.Accounts.Presentation.Requests;
-
-public record LoginUserRequest(string Email, string Password)
-{
-    public LoginUserCommand ToCommand()
-        => new(Email, Password);
-}
--- backend/src/Accounts/AnimalAllies.Accounts.Presentation/Requests/RegisterUserRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using AnimalAllies.Accounts.Application.AccountManagement.Commands.Register;
-
-namespace AnimalAllies.Accounts.Presentation.Requests;
-
-public record RegisterUserRequest(string Email, string UserName, string Password)
-{
-    public RegisterUserCommand ToCommand()
-        => new(Email, UserName, Password);
-}
