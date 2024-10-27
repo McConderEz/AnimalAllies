6f6b09df5e05bd6085b245eef38e90215d82da84
McConderEz <kamazitun.jaykehd@gmail.com>
2024-10-27 15:10:19 +0300

    added discussion domain, added tests for BL

--- backend/src/Discussion/Discussion.Domain/ValueObjects/DiscussionStatus.cs
+++ backend/src/Discussion/Discussion.Domain/ValueObjects/DiscussionStatus.cs
@@ -33,6 +33,6 @@ public class DiscussionStatus : ValueObject
     
     protected override IEnumerable<object> GetEqualityComponents()
     {
-        throw new NotImplementedException();
+        yield return Value;
     }
 }
---
+++ backend/src/Discussion/Discussion.Domain/ValueObjects/Users.cs
@@ -0,0 +1,31 @@
+﻿using AnimalAllies.SharedKernel.Shared;
+
+namespace Discussion.Domain.ValueObjects;
+
+public class Users: ValueObject
+{
+    public Guid FirstUserId { get; }
+    public Guid SecondUserId { get; }
+    
+    private Users(){}
+
+    private Users(Guid firstUserId, Guid secondUserId)
+    {
+        FirstUserId = firstUserId;
+        SecondUserId = secondUserId;
+    }
+
+    public static Result<Users> Create(Guid firstUserId, Guid secondUserId)
+    {
+        if (firstUserId == Guid.Empty || secondUserId == Guid.Empty)
+            return Errors.General.Null("one of users ids");
+
+        return new Users(firstUserId, secondUserId);
+    }
+    
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return FirstUserId;
+        yield return SecondUserId;
+    }
+}
