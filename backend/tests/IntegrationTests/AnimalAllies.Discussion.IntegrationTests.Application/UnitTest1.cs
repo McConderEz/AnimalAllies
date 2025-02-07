cd5358d0f066fdb1b8370231a4c0f779f4833ece
McConderEz <kamazitun.jaykehd@gmail.com>
2025-02-07 22:26:46 +0300

    added projects for integration tests

---
+++ backend/tests/IntegrationTests/AnimalAllies.Discussion.IntegrationTests.Application/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimalAllies.Discussion.IntegrationTests.Application;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ backend/tests/IntegrationTests/AnimalAllies.Volunteer.IntegrationTests.Application/UnitTest1.cs
@@ -0,0 +1,9 @@
+﻿namespace AnimalAllies.Volunteer.IntegrationTests.Application;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
